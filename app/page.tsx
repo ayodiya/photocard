c03502193bec2d3476a72b73dc18a0576d87a3d5
ayodiya <devayodiya@gmail.com>
2024-08-30 14:12:51 +0100

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import Box from "@mui/material/Box";
+"use client";
+
+import Container from "@mui/material/Container";
+import { useState } from "react";
+
+import PhotoCard from "@/components/PhotoCard";
 
 export default function Home() {
-  return <Box>Testing</Box>;
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        paddingTop: "50px",
+        paddingBottom: "50px",
+      }}
+    >
+      <PhotoCard />
+    </Container>
+  );
 }
